3d9612b0e30ee017bf7123a0b164f2ed894c305f
sholehbudi1999@gmail.com <sholehbudi-utomo>
2023-02-26 01:46:31 +0700

    tugas2

---
+++ index.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">
+          
+
+    <title>My personal web</title>
+  </head>
+  <body class="bg-secondary">
+    <div class="container mt-5 mb-5">
+        <div class="card text-center shadow mb-5">     
+            <div class="card-body">
+                <img src="asset/profil.jpg" class="rounded-circle w-50"/>
+                <h1 class="mb-0">Sholeh Budi Utomo</h1>
+                <span class="fw-light d-block mb-3">Web Developer</span>
+                <p class="card-text">Start from a dream because it will affect your goals</p>
+            </div>
+            <div class="card-footer bg-white fs-4 ">
+            <a href="https://facebook.com/sholehbudiutomo" class="text-secondary"><i class="fa-brands fa-square-facebook"></i></a>
+            <a href="https://instagram.com/sholehbudiutomo" class="text-secondary"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://www.youtube.com/@gaeweb3855" class="text-secondary" ><i class="fa-brands fa-square-youtube"></i></a>
+            </div>
+        </div>
+
+        <!-- kol 2 -->
+       <div class="card text-center shadow mb-5">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              <div class="row">
+                <div class="col-3">Email</div>
+                <div class="col-8 text-end text-muted">sholehbudi1999@gmail.com</div>
+              </div>
+            </li>
+            <li class="list-group-item">
+            <div class="row">
+                <div class="col-3">Address</div>
+                <div class="col-8 text-end text-muted">Sukoharjo, Central Java, Indonesia</div>
+              </div>
+            </li>
+            <li class="list-group-item">
+            <div class="row">
+                <div class="col-3">GitHub</div>
+                <div class="col-8 text-end text-muted"><a href="https://github.com/sholehbudi-utomo">sholehbudi-utomo</a></div>
+              </div>
+            </li>
+            <li class="list-group-item">
+            <div class="row">
+                <div class="col-3">Phone</div>
+                <div class="col-8 text-end text-muted">089602677810</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+      <!-- kol 3 -->
+      <div class=" card text-center shadow mb-5">
+
+        <div class="card text-center bg-light fw-bold">
+        Programming Language 
+        </div>
+        <div class="card-body">
+          <div class="row row-cols-4 fs-1 text-center">
+            <div class="col"><i class="devicon-html5-plain-wordmark colored"></i></div>
+            <div class="col"><i class="devicon-css3-plain-wordmark colored"></i></div>
+            <div class="col"><i class="devicon-javascript-plain colored"></i></div>
+            <div class="col"><i class="devicon-tailwindcss-plain colored"></i></div>
+          </div>
+
+          <div class="row row-cols-4 fs-1 text-center">
+            <div class="col"><i class="devicon-php-plain colored"></i></div>
+            <div class="col"><i class="devicon-mysql-plain-wordmark colored"></i></div>
+            <div class="col"><i class="devicon-laravel-plain-wordmark colored"></i></div>
+            <div class="col"><i class="devicon-bootstrap-plain colored"></i></div>
+          </div>
+        </div>
+        
+      </div>
+
+      <!-- kol 4 -->
+
+      <div class="card">
+        <div class="card text-center bg-light fw-bold"> Soft Skills </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              <p>Problem Solving</p>
+              <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 75%"></div>
+              </div>
+            </li>
+            <li class="list-group-item">
+              <p>Comunication</p>
+              <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 75%"></div>
+              </div>
+            </li>
+            <li class="list-group-item">
+              <p>Team Work</p>
+              <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 75%"></div>
+              </div>
+            </li>
+            <li class="list-group-item">
+              <p>Time Management</p>
+              <div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 75%"></div>
+              </div>
+            </li>
+          </ul>
+          <div class="card-footer bs-light">
+          </div>
+      </div>
+
+    </div>
+
+    
+
+    <!-- js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
